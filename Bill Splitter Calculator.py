ba3d8f0a1423f77f166dfac89b2923f21c0d0832
Jeremiah <jeremiahtaylor2017@gmail.com>
2021-09-18 03:27:44 -0700

    Added functionality

--- Bill Splitter Calculator.py
+++ Bill Splitter Calculator.py
@@ -1,16 +1,25 @@
 # Bill Splitter Calculator
 
-n_people = int(input("Enter how many people joining you (including you): "))
+n_people = int(input("Enter number of people joining you (including you): "))
 print()
 
 names_dict = {}
 
-if n_people > 0:
+if n_people <= 0:
+    print("No one is joining for the party")
+else:
     print("Enter the names of everyone (including you), each on a new line.")
+
     for i in range(n_people):
         name = input()
         names_dict[name] = 0
     print()
+
+    bill = float(input("Enter the total bill value: "))
+
+    split = round(bill / n_people, 2)
+
+    names_dict = names_dict.fromkeys(names_dict, split)
+
+    print()
     print(names_dict)
-else:
-    print("No one is joining for the party")
