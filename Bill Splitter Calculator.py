3be073fb2f81328eba60d3669867023e0bd5f262
Jeremiah <jeremiahtaylor2017@gmail.com>
2021-09-20 02:28:10 -0700

    Added who's lucky feature

--- Bill Splitter Calculator.py
+++ Bill Splitter Calculator.py
@@ -1,4 +1,5 @@
 # Bill Splitter Calculator
+from random import choice
 
 n_people = int(input("Enter number of people joining you (including you): "))
 print()
@@ -10,9 +11,10 @@ if n_people <= 0:
 else:
     print("Enter the names of everyone (including you), each on a new line.")
 
-    for i in range(n_people):
+    for _ in range(n_people):
         name = input()
         names_dict[name] = 0
+
     print()
 
     bill = float(input("Enter the total bill value: "))
@@ -22,4 +24,14 @@ else:
     names_dict = names_dict.fromkeys(names_dict, split)
 
     print()
-    print(names_dict)
+    # print(names_dict)
+
+    lucky_input = input(
+        "Do you want to use the \"Who's luck\" feature? Please enter y/n . ").strip()
+    if lucky_input == "Yes":
+        lucky_person = choice(list(names_dict))
+        print()
+        print(f'{lucky_person} is the lucky one!')
+    else:
+        print()
+        print("No one is going to be lucky")
